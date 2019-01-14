65ca15dada826aa5a945d267792d7876a7bec4c5
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-01-14 16:01:44 -0300

    test: check all required field testeds

--- src/utils/form-validator/__tests__/form-validator.spec.js
+++ src/utils/form-validator/__tests__/form-validator.spec.js
@@ -23,248 +23,75 @@ let formValue = {
 describe('FORM: all params required', () => {
 
   describe('when not inform the name', () => {
+    let form = { ...formValue };
 
-//    formValue = formHelper.updateForm(formValue, 'name', 'joao');
-    formValue = formHelper.updateForm(formValue, 'email', 'joao@email.com');
-    formValue = formHelper.updateForm(formValue, 'password', '1234Asdf');
-    formValue = formHelper.updateForm(formValue, 'confirmPassword', '1234Asdf');
+    form = formHelper.updateForm(form, 'name', '');
+    form = formHelper.updateForm(form, 'email', 'joao@email.com');
+    form = formHelper.updateForm(form, 'password', '1234Asdf');
+    form = formHelper.updateForm(form, 'confirmPassword', '1234Asdf');
 
-    
     it('should return name is required', () => {
-      const valid = isValidForm(formValue);
+      const valid = isValidForm(form);
 
       expect(valid).toBeFalsy();
-      expect(formValue.name.errors.required).toBeTruthy();
-      expect(formValue.name.valid).toBeFalsy();
+      expect(form.name.errors.required).toBeTruthy();
+      expect(form.name.valid).toBeFalsy();
     });
 
   });
 
+  describe('when not inform the email', () => {
 
-  /* describe('when form init INVALID on email', () => {
-    let formValue;
+    let form = { ...formValue };
+    form = formHelper.updateForm(form, 'name', 'joao');
+    form = formHelper.updateForm(form, 'email', '');
+    form = formHelper.updateForm(form, 'password', '1234Asdf');
+    form = formHelper.updateForm(form, 'confirmPassword', '1234Asdf');
 
-    beforeEach(() => {
-      formValue = {
-        name: formHelper.buildParam({
-          value: 'joao',
-          validators: [requiredValidator]
-        }),
-        email: formHelper.buildParam({
-          value: 'joa',
-          validators: [requiredValidator, emailValidator]
-        }),
-        password: formHelper.buildParam({
-          value: '123456AAAaaa',
-          validators: [requiredValidator]
-        }),
-        confirmPassword: formHelper.buildParam({
-          value: '123456AAAaaa',
-          validators: [requiredValidator]
-        })
-      }
+    it('should return email is required', () => {
+      const valid = isValidForm(form);
+
+      expect(valid).toBeFalsy();
+      expect(form.email.errors.required).toBeTruthy();
+      expect(form.email.valid).toBeFalsy();
     });
 
-    describe('when all form are VALID', () => {
-      it('should return a valid form', () => {
-        expect(isValidForm(formValue)).toBeFalsy();
-      });
-    });
   });
 
+  describe('when not inform the password', () => {
 
-  describe('when form init INVALID on required', () => {
-    let formValue;
+    let form = { ...formValue };
+    form = formHelper.updateForm(form, 'name', 'joao');
+    form = formHelper.updateForm(form, 'email', 'joao@email.com');
+    form = formHelper.updateForm(form, 'password', '');
+    form = formHelper.updateForm(form, 'confirmPassword', '1234Asdf');
 
-    beforeEach(() => {
-      formValue = {
-        name: formHelper.buildParam({
-          value: '',
-          validators: [requiredValidator]
-        }),
-        email: formHelper.buildParam({
-          value: 'joa@email.com',
-          validators: [requiredValidator, emailValidator]
-        }),
-        password: formHelper.buildParam({
-          value: '123456AAAaaa',
-          validators: [requiredValidator]
-        }),
-        confirmPassword: formHelper.buildParam({
-          value: '123456AAAaaa',
-          validators: [requiredValidator]
-        })
-      }
+    it('should return password is required', () => {
+      const valid = isValidForm(form);
+
+      expect(valid).toBeFalsy();
+      expect(form.password.errors.required).toBeTruthy();
+      expect(form.password.valid).toBeFalsy();
     });
 
-    describe('when all form are VALID', () => {
-      it('should return a valid form', () => {
-        expect(isValidForm(formValue)).toBeFalsy();
-      });
-    });
   });
 
+  describe('when not inform the confirmPassword', () => {
 
+    let form = { ...formValue };
+    form = formHelper.updateForm(form, 'name', 'joao');
+    form = formHelper.updateForm(form, 'email', 'joao@email.com');
+    form = formHelper.updateForm(form, 'password', '');
+    form = formHelper.updateForm(form, 'confirmPassword', '');
 
-  describe('when form init INVALID on Password Min', () => {
-    let formValue;
+    it('should return confirmPassword is required', () => {
+      const valid = isValidForm(form);
 
-    beforeEach(() => {
-      formValue = {
-        name: formHelper.buildParam({
-          value: 'joao',
-          validators: [requiredValidator]
-        }),
-        email: formHelper.buildParam({
-          value: 'joa@email.com',
-          validators: [requiredValidator, emailValidator]
-        }),
-        password: formHelper.buildParam({
-          value: '1aaa',
-          validators: [requiredValidator]
-        }),
-        confirmPassword: formHelper.buildParam({
-          value: '1aaaa',
-          validators: [requiredValidator]
-        })
-      }
+      expect(valid).toBeFalsy();
+      expect(form.confirmPassword.errors.required).toBeTruthy();
+      expect(form.confirmPassword.valid).toBeFalsy();
     });
 
-    describe('when all form are VALID', () => {
-      it('should return a valid form', () => {
-        expect(isValidForm(formValue)).toBeFalsy();
-      });
-    });
   });
-
-  describe('when form init INVALID on PASSWORD no captial case', () => {
-    let formValue;
-
-    beforeEach(() => {
-      formValue = {
-        name: formHelper.buildParam({
-          value: 'joao',
-          validators: [requiredValidator]
-        }),
-        email: formHelper.buildParam({
-          value: 'joa@email.com',
-          validators: [requiredValidator, emailValidator]
-        }),
-        password: formHelper.buildParam({
-          value: 'asdfgh123456',
-          validators: [requiredValidator]
-        }),
-        confirmPassword: formHelper.buildParam({
-          value: 'asdfgh123456',
-          validators: [requiredValidator]
-        })
-      }
-    });
-
-    describe('when all form are VALID', () => {
-      it('should return a valid form', () => {
-        expect(isValidForm(formValue)).toBeFalsy();
-      });
-    });
-  });
-
-
-
-  describe('when form init INVALID on pPASSWORD no number', () => {
-    let formValue;
-
-    beforeEach(() => {
-      formValue = {
-        name: formHelper.buildParam({
-          value: 'joao',
-          validators: [requiredValidator]
-        }),
-        email: formHelper.buildParam({
-          value: 'joa@email.com',
-          validators: [requiredValidator, emailValidator]
-        }),
-        password: formHelper.buildParam({
-          value: 'asdfghj',
-          validators: [requiredValidator]
-        }),
-        confirmPassword: formHelper.buildParam({
-          value: 'asdfghj',
-          validators: [requiredValidator]
-        })
-      }
-    });
-
-    describe('when all form are VALID', () => {
-      it('should return a valid form', () => {
-        expect(isValidForm(formValue)).toBeFalsy();
-      });
-    });
-  });
-
-
-
-  describe('when form init INVALID on PASSWORD no letters', () => {
-    let formValue;
-
-    beforeEach(() => {
-      formValue = {
-        name: formHelper.buildParam({
-          value: 'joao',
-          validators: [requiredValidator]
-        }),
-        email: formHelper.buildParam({
-          value: 'joa@email.com',
-          validators: [requiredValidator, emailValidator]
-        }),
-        password: formHelper.buildParam({
-          value: '12345678',
-          validators: [requiredValidator]
-        }),
-        confirmPassword: formHelper.buildParam({
-          value: '12345678',
-          validators: [requiredValidator]
-        })
-      }
-    });
-
-    describe('when all form are VALID', () => {
-      it('should return a valid form', () => {
-        expect(isValidForm(formValue)).toBeFalsy();
-      });
-    });
-  });
-
-
-
-  describe('when form init INVALID on PASSWORD no length', () => {
-    let formValue;
-
-    beforeEach(() => {
-      formValue = {
-        name: formHelper.buildParam({
-          value: 'joao',
-          validators: [requiredValidator]
-        }),
-        email: formHelper.buildParam({
-          value: 'joa@email.com',
-          validators: [requiredValidator, emailValidator]
-        }),
-        password: formHelper.buildParam({
-          value: 'Asd12',
-          validators: [requiredValidator]
-        }),
-        confirmPassword: formHelper.buildParam({
-          value: 'Asd12',
-          validators: [requiredValidator]
-        })
-      }
-    });
-
-    describe('when all form are VALID', () => {
-      it('should return a valid form', () => {
-        expect(isValidForm(formValue)).toBeFalsy();
-      });
-    });
-  }); */
 
 });
